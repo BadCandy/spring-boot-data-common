da8e4987d2430a3a732d77ef13d0693a6dc8f005
badcandy <christ9979@naver.com>
2018-11-06 23:11:22 +0900

    feat(Application): Make spring-jpa frame.
    Make spring-jpa frame.
    
    BREAKING CHANGE: X

---
+++ src/main/java/me/christ9979/springbootjpa/JpaRunner.java
@@ -0,0 +1,28 @@
+package me.christ9979.springbootjpa;
+
+import me.christ9979.springbootjpa.account.Account;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Transactional
+@Component
+public class JpaRunner implements ApplicationRunner {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        Account account = new Account();
+        account.setUsername("christ9979");
+        account.setPassword("pass");
+
+        entityManager.persist(account);
+    }
+}
